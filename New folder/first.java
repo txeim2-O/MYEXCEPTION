6ca3261cd170f95c9f8be3bb46a4d94f91726417
txeim <txeimcode@gmail.com>
2026-06-30 14:16:04 +0530

    using if else

---
+++ New folder/first.java
@@ -0,0 +1,8 @@
+public class first {
+    public static void main(String [] args){
+        int age=19;
+        if (age>18){
+            System.out.println("Age is greater than 18");
+        }else System.out.println("Age is less than 18");
+    }
+}
