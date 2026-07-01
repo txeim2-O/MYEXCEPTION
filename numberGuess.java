3261c4a7c7a27e044f801e09bc4aa7d676f6df9e
txeim <txeimcode@gmail.com>
2026-07-01 20:42:43 +0530

    complete

--- numberGuess.java
+++ numberGuess.java
@@ -8,26 +8,30 @@ public class numberGuess {
         int randint=random.nextInt(1,101);
         int counter=1;
         try{
-            System.out.println("Enter your number");
+            System.out.print("Enter your number between 1 to 100");
             int num=input.nextInt();
             if(num==randint){
                 System.out.println("Your have found the number in your 1th chance");
-                 
             }
-        while(num!=randint || num==randint){
-            if(num>randint){
-                System.out.println("Select a number smaller than "+num);
-            }else if(num<randint){
-                System.out.println("Select a number greater than "+num);
-            
-            num=input.nextInt();
+            else{
+                while(num!=randint){
+                    if(num>randint){
+                        System.out.println("Select a number smaller than "+num);
+                    }else if(num<randint){
+                        System.out.println("Select a number greater than "+num);
+                    }
+                    num=input.nextInt();
+                    counter++;
+                } 
+                System.out.println("Your have found the number in your "+counter+" th chnace");
             }
-        } 
-        System.out.println("Your have found the number int your "+counter+" th chnace");
         }catch(InputMismatchException e){
             System.out.println("Input should be a number");
         }
+ 
         
     }
-    
+   
 }
+
+ 
